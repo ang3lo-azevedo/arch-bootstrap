f69ffd1afac41e2ed32bf4427ed77961c8460b5a
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-05-02 14:08:56 +0100

    Enhance arch-bootstrap script to install git, clone the repository, and update submodules; streamline sourcing of utils and menu scripts.

--- arch-bootstrap.sh
+++ arch-bootstrap.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-dir="$(dirname "$0")"
+# Install dependencies
+echo "Installing git"
+sudo pacman -S --needed --noconfirm git
+
+# Clone the repository
+echo "Cloning the repository"
+git clone https://github.com/ang3lo-azevedo/arch-bootstrap.git
+
+# Change to the repository directory
+cd arch-bootstrap
+
+# Get the submodules
+git submodule update --init --recursive
 
 # Source utils
-source "$dir/utils/utils.sh"
+source "utils/utils.sh"
 
 # Source menu
-source "$dir/whiptail_menus/main_menu.sh"
+source "whiptail_menus/main_menu.sh"
 
 # Run menu
 menu

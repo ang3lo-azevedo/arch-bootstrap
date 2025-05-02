c70c965d8d0a172178b1033722c027379a06c90c
50565 - Ângelo Azevedo <a50565@alunos.isel.pt>
2025-05-02 20:18:08 +0100

    Refactor loadkeys_menu function in loadkeys_menu.sh to check keyboard layout using localectl instead of loadkeys for improved accuracy in layout verification.

--- dialog_menus/loadkeys/loadkeys_menu.sh
+++ dialog_menus/loadkeys/loadkeys_menu.sh
@@ -3,7 +3,7 @@
 # Function to show the loadkeys menu
 loadkeys_menu() {
     # Check if pt-latin1 is loaded
-    if ! (loadkeys | grep -q "pt-latin1"); then
+    if ! (localectl status | grep "pt-latin1"); then
         yes_no_menu "Load pt-latin1 keyboard layout?"
         if [ $? -eq 0 ]; then
             loadkeys pt-latin1
